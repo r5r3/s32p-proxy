7a2c1c253bfbfbaad8b0875944fdc865b9bf9cf8
Robert Redl <robert.redl@lmu.de>
2026-05-24 17:33:43 +0200

    cleanup

--- test-env/run-versitygw.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#
-../../versity-patched/versitygw --debug --iam-vault-endpoint-url http://127.0.0.1:8200 --iam-vault-root-token root --iam-vault-secret-storage-path versitygw --iam-vault-mount-path secret --access test --secret test posix /tmp/gw
